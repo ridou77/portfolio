52bbaa99310b798c596277498629425e618ae3ea
Rémi RUSSI <remi.russi@ecole-isitech.fr>
2025-04-28 11:53:45 +0200

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const sidebar = document.getElementById("sidebar");
+const sidebarToggle = document.getElementById("sidebarToggle");
+const menuLinks = sidebar.querySelectorAll("a");
+
+sidebarToggle.addEventListener("click", () => {
+    sidebarToggle.classList.toggle("active");
+    sidebar.classList.toggle("active");
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+        sidebarToggle.classList.remove("active");
+    });
+});
